0b411d4bbf02f769c2a2ed479cd1390e3fbbeaaf
Karnasula Jaya sai venkatesh <jayasaivenkateshkarnasula@gmail.com>
2026-03-10 11:32:57 +0530

    Add files via upload

---
+++ build/CMakeFiles/chronos_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chronos_server.dir/src/Database.cpp.o"
+  "CMakeFiles/chronos_server.dir/src/Database.cpp.o.d"
+  "CMakeFiles/chronos_server.dir/src/main.cpp.o"
+  "CMakeFiles/chronos_server.dir/src/main.cpp.o.d"
+  "chronos_server"
+  "chronos_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chronos_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
